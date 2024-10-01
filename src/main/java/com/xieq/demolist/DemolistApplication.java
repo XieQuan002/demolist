9dc0d96927e1f7a51c6271b628398a9c2ef99a85
xiequan <2642354206@qq.com>
2024-10-01 21:18:29 +0800

    first

---
+++ src/main/java/com/xieq/demolist/DemolistApplication.java
@@ -0,0 +1,14 @@
+package com.xieq.demolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolistApplication.class, args);
+
+    }
+
+}
